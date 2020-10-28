d993690982ae1d52cf2a98a7fbfedec9d3373d4b
Dave Hoy <hoyski@gmail.com>
2020-10-28 10:25:42 -0400

    Removed sum methods

--- SomeHugeCorporateProject/src/com/hugecorp/util/MathUtils.java
+++ SomeHugeCorporateProject/src/com/hugecorp/util/MathUtils.java
@@ -1,11 +1,5 @@
 package com.hugecorp.util;
 
 public class MathUtils {
-	public int sum(int a, int b) {
-		return a + b;
-	}
-	
-	public int sum(int a, int b, int c, int d) {
-		return a + b + c + d;
-	}
+
 }
