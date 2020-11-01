e3cbe5d288ba0dc25143fdc05bb2425eb4ed719c
Chris Mason <m1achris43@gmail.com>
2020-11-01 08:57:00 -0500

    Added Return 4 ints

--- SomeHugeCorporateProject/src/com/hugecorp/util/MathUtils.java
+++ SomeHugeCorporateProject/src/com/hugecorp/util/MathUtils.java
@@ -8,4 +8,8 @@ public class MathUtils {
 	public static int sum(int a, int b, int c) {
 		return a + b + c;
 	}
+	
+	public static int sum(int a, int b, int c, int d) {
+		return a + b + c + d;
+	}
 }
