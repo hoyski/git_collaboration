bb69ae0418a0b95746f5ef28d4ea31bbaa0fd0e6
Dave Hoy <hoyski@gmail.com>
2020-11-01 09:11:02 -0500

    Resolved conflicts

--- SomeHugeCorporateProject/src/com/hugecorp/util/MathUtils.java
+++ SomeHugeCorporateProject/src/com/hugecorp/util/MathUtils.java
@@ -1,19 +1,23 @@
 package com.hugecorp.util;
 
 public class MathUtils {
-	public static int sum (int a, int b) {
+	public static int sum(int a, int b) {
 		return a + b;
 	}
-	
+
 	public static int sum(int a, int b, int c) {
 		return a + b + c;
 	}
-	
+
 	public static int sum(int a, int b, int c, int d) {
 		return a + b + c + d;
 	}
-	
+
 	public static int sum(int a, int b, int c, int d, int e) {
 		return a + b + c + d + e;
+	}
+
+	public static int sum(int a, int b, int c, int d, int e, int f) {
+		return a + b + c + d + e + f;
 	}
 }
